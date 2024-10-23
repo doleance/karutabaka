915e61541df97e80d69aa2f2b3de0eae4588e23d
Ivett LATÁK <ivett.latak@shiwaforce.com>
2024-10-23 13:45:56 +0200

    feat: use favicon based on dark or light theme

--- karutabaka/src/App.vue
+++ karutabaka/src/App.vue
@@ -1,16 +1,20 @@
-<script setup lang="ts">
-import { ROUTE_NAMES } from './router/routes';
-</script>
-
 <template>
-  <nav>
-    <RouterLink :to="{name: ROUTE_NAMES.HOME}">Go Home</RouterLink>
-    <RouterLink :to="{name: ROUTE_NAMES.RECITER}">Read please</RouterLink>
-  </nav>
-  <main>
-    <RouterView />
-  </main>
+	<nav>
+		<RouterLink :to="{ name: ROUTE_NAMES.HOME }">Go Home</RouterLink>
+		<RouterLink :to="{ name: ROUTE_NAMES.RECITER }">Read please</RouterLink>
+	</nav>
+	<main>
+		<RouterView />
+	</main>
 </template>
 
-<style scoped>
-</style>
+<script setup lang="ts">
+import { ROUTE_NAMES } from './router/routes';
+import { useFavicon, usePreferredDark } from '@vueuse/core';
+import { computed } from 'vue';
+
+const isDark = usePreferredDark();
+const favicon = computed(() => (isDark.value ? 'favicon-momiji.ico' : 'favicon.ico'));
+
+useFavicon(favicon);
+</script>
