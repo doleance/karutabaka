dbe64bde7b307e40b89dc6d35c9399dfb87eb71f
Ivett LATÁK <ivett.latak@shiwaforce.com>
2024-10-27 10:49:30 +0100

    feat: create karuta cards array

---
+++ karutabaka/src/constants/karuta-cards.ts
@@ -0,0 +1,406 @@
+import { KarutaCard } from '../interfaces/karuta-card.interface';
+
+const KARUTA_CARD_ASSET_PATH = './public/assets/images/karuta-cards';
+
+export const KARUTA_CARDS: KarutaCard[] = [
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/01_akino.jpg`,
+		identifyingCharacters: 'akino',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/02_harusu.jpg`,
+		identifyingCharacters: 'harusu',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/03_ashi.jpg`,
+		identifyingCharacters: 'ashi',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/04_tago.jpg`,
+		identifyingCharacters: 'tago',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/05_oku.jpg`,
+		identifyingCharacters: 'oku',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/06_kasa.jpg`,
+		identifyingCharacters: 'kasa',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/07_amano.jpg`,
+		identifyingCharacters: 'amano',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/08_wagai.jpg`,
+		identifyingCharacters: 'wagai',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/09_hanano.jpg`,
+		identifyingCharacters: 'hanano',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/10_kore.jpg`,
+		identifyingCharacters: 'kore',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/11_watanoharaya.jpg`,
+		identifyingCharacters: 'watanoharaya',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/12_amatsu.jpg`,
+		identifyingCharacters: 'amatsu',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/13_tsuku.jpg`,
+		identifyingCharacters: 'tsuku',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/14_michi.jpg`,
+		identifyingCharacters: 'michi',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/15_kimigatameha.jpg`,
+		identifyingCharacters: 'kimigatameha',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/16_tachi.jpg`,
+		identifyingCharacters: 'tachi',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/17_chiha.jpg`,
+		identifyingCharacters: 'chiha',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/18_su.jpg`,
+		identifyingCharacters: 'su',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/19_naniwaga.jpg`,
+		identifyingCharacters: 'naniwaga',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/20_wabi.jpg`,
+		identifyingCharacters: 'wabi',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/21_imako.jpg`,
+		identifyingCharacters: 'imako',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/22_fu.jpg`,
+		identifyingCharacters: 'fu',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/23_tsuki.jpg`,
+		identifyingCharacters: 'tsuki',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/24_kono.jpg`,
+		identifyingCharacters: 'kono',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/25_nanishi.jpg`,
+		identifyingCharacters: 'nanishi',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/26_ogu.jpg`,
+		identifyingCharacters: 'ogu',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/27_mikano.jpg`,
+		identifyingCharacters: 'mikano',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/28_yamaza.jpg`,
+		identifyingCharacters: 'yamaza',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/29_kokoroa.jpg`,
+		identifyingCharacters: 'kokoroa',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/30_aria.jpg`,
+		identifyingCharacters: 'aria',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/31_asaborakea.jpg`,
+		identifyingCharacters: 'asaborakea',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/32_yamaga.jpg`,
+		identifyingCharacters: 'yamaga',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/33_hisa.jpg`,
+		identifyingCharacters: 'hisa',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/34_tare.jpg`,
+		identifyingCharacters: 'tare',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/35_hitowa.jpg`,
+		identifyingCharacters: 'hitowa',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/36_natsu.jpg`,
+		identifyingCharacters: 'natsu',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/37_shira.jpg`,
+		identifyingCharacters: 'shira',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/38_wasura.jpg`,
+		identifyingCharacters: 'wasura',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/39_asaji.jpg`,
+		identifyingCharacters: 'asaji',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/40_shino.jpg`,
+		identifyingCharacters: 'shino',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/41_koi.jpg`,
+		identifyingCharacters: 'koi',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/42_chigiriki.jpg`,
+		identifyingCharacters: 'chigiriki',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/43_ai.jpg`,
+		identifyingCharacters: 'ai',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/44_ooko.jpg`,
+		identifyingCharacters: 'ooko',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/45_aware.jpg`,
+		identifyingCharacters: 'aware',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/46_yura.jpg`,
+		identifyingCharacters: 'yura',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/47_yae.jpg`,
+		identifyingCharacters: 'yae',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/48_kazeo.jpg`,
+		identifyingCharacters: 'kazeo',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/49_mikaki.jpg`,
+		identifyingCharacters: 'mikaki',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/50_kimigatameo.jpg`,
+		identifyingCharacters: 'kimigatameo',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/51_kaku.jpg`,
+		identifyingCharacters: 'kaku',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/52_ake.jpg`,
+		identifyingCharacters: 'ake',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/53_nageki.jpg`,
+		identifyingCharacters: 'nageki',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/54_wasure.jpg`,
+		identifyingCharacters: 'wasure',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/55_taki.jpg`,
+		identifyingCharacters: 'taki',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/56_araza.jpg`,
+		identifyingCharacters: 'araza',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/57_me.jpg`,
+		identifyingCharacters: 'me',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/58_arima.jpg`,
+		identifyingCharacters: 'arima',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/59_yasu.jpg`,
+		identifyingCharacters: 'yasu',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/60_ooe.jpg`,
+		identifyingCharacters: 'ooe',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/61_ini.jpg`,
+		identifyingCharacters: 'ini',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/62_yoo.jpg`,
+		identifyingCharacters: 'yoo',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/63_imawa.jpg`,
+		identifyingCharacters: 'imawa',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/64_asaborakeu.jpg`,
+		identifyingCharacters: 'asaborakeu',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/65_ura.jpg`,
+		identifyingCharacters: 'ura',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/66_moro.jpg`,
+		identifyingCharacters: 'moro',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/67_haruno.jpg`,
+		identifyingCharacters: 'haruno',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/68_kokoroni.jpg`,
+		identifyingCharacters: 'kokoroni',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/69_arashi.jpg`,
+		identifyingCharacters: 'arashi',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/70_sa.jpg`,
+		identifyingCharacters: 'sa',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/71_yuu.jpg`,
+		identifyingCharacters: 'yuu',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/72_oto.jpg`,
+		identifyingCharacters: 'oto',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/73_taka.jpg`,
+		identifyingCharacters: 'taka',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/74_uka.jpg`,
+		identifyingCharacters: 'uka',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/75_chigirio.jpg`,
+		identifyingCharacters: 'chigirio',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/76_watanoharako.jpg`,
+		identifyingCharacters: 'watanoharako',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/77_se.jpg`,
+		identifyingCharacters: 'se',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/78_awaji.jpg`,
+		identifyingCharacters: 'awaji',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/79_akika.jpg`,
+		identifyingCharacters: 'akika',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/80_nagaka.jpg`,
+		identifyingCharacters: 'nagaka',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/81_ho.jpg`,
+		identifyingCharacters: 'ho',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/82_omo.jpg`,
+		identifyingCharacters: 'omo',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/83_yononakayo.jpg`,
+		identifyingCharacters: 'yononakayo',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/84_nagara.jpg`,
+		identifyingCharacters: 'nagara',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/85_yomo.jpg`,
+		identifyingCharacters: 'yomo',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/86_nageke.jpg`,
+		identifyingCharacters: 'nageke',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/87_mu.jpg`,
+		identifyingCharacters: 'mu',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/88_naniwae.jpg`,
+		identifyingCharacters: 'naniwae',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/89_tama.jpg`,
+		identifyingCharacters: 'tama',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/90_mise.jpg`,
+		identifyingCharacters: 'mise',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/91_kiri.jpg`,
+		identifyingCharacters: 'kiri',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/92_wagaso.jpg`,
+		identifyingCharacters: 'wagaso',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/93_yononakawa.jpg`,
+		identifyingCharacters: 'yononakawa',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/94_miyo.jpg`,
+		identifyingCharacters: 'miyo',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/95_ooke.jpg`,
+		identifyingCharacters: 'ooke',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/96_hanasa.jpg`,
+		identifyingCharacters: 'hanasa',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/97_konu.jpg`,
+		identifyingCharacters: 'konu',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/98_kazeso.jpg`,
+		identifyingCharacters: 'kazeso',
+	},
+	{
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/99_hitomo.jpg`,
+		identifyingCharacters: 'hitomo',
+	},
+	{
+		identifyingCharacters: 'momo',
+		imageSrc: `${KARUTA_CARD_ASSET_PATH}/00_momo.jpg`,
+	},
+];
---
+++ karutabaka/src/interfaces/karuta-card.interface.ts
@@ -0,0 +1,7 @@
+export interface KarutaCard {
+	readonly identifyingCharacters: string;
+	readonly imageSrc: string;
+	// readonly soundSrc: string;
+	// readonly verseFirstPart: string;
+	// readonly verseSecondPart: string;
+}
