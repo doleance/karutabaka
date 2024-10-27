d0eb1abc07c105173a533c2d5811edb975df0fe2
Attila Szőke <szokeattila9801@gmail.com>
2024-10-27 10:40:29 +0100

    feat: home card component sitebuild

---
+++ karutabaka/src/components/HomeCard.vue
@@ -0,0 +1,53 @@
+<template>
+	<RouterLink to="to" class="navigation-tile" aria-labelledby="tile-title">
+		<div class="tile-container">
+			<h3 id="tile-title" class="tile-title">Tile Title</h3>
+			<div class="tile-content">
+				<img src="/assets/images/bara-head.webp" height="148" width="180" alt="" />
+				<p class="tile-description">Short description of the destination page.</p>
+			</div>
+		</div>
+	</RouterLink>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/styles/colors' as *;
+
+.navigation-tile {
+	background: $card-background;
+	border-radius: 16px;
+	box-shadow: $box-shadow-card;
+	color: $text-color-main;
+	display: block;
+	height: 428px;
+	margin: 20px;
+	max-width: 248px;
+	padding: 20px;
+	text-decoration: none;
+
+	& .tile-container {
+		border-radius: 4px;
+		border: 2px $text-color-main solid;
+		height: 100%;
+		width: 100%;
+
+		& .tile-title {
+			font-size: 40px;
+			text-align: center;
+		}
+
+		& .tile-content {
+			border-top: 2px $text-color-main solid;
+			display: flex;
+			flex-direction: column;
+			gap: 40px;
+			padding: 32px 12px;
+
+			& .tile-description {
+				font-size: 16px;
+				text-align: center;
+			}
+		}
+	}
+}
+</style>
--- karutabaka/src/components/PageHeader.vue
+++ karutabaka/src/components/PageHeader.vue
@@ -1,9 +1,9 @@
 <template>
-	<header class="header">
+	<header class="page-header">
 		<div class="header-column">
 			<img :src="imgSrc" alt="" height="44px" width="44" />
 		</div>
-		<nav class="nav">
+		<nav class="page-navigation">
 			<RouterLink v-for="(route, index) in routesToShow" :key="index" class="link" :to="{ name: route.name }">
 				{{ route.meta?.navTitle }}
 			</RouterLink>
@@ -20,47 +20,46 @@ import { usePreferredDark } from '@vueuse/core';
 const routesToShow = computed(() => routes.filter((route) => route.meta?.navTitle));
 
 const isDark = usePreferredDark();
-const imgSrc = computed(() => `/assets/images/patterns/bara-head${isDark.value ? '-momiji' : ''}.webp`);
+const imgSrc = computed(() => `/assets/images/bara-head${isDark.value ? '-momiji' : ''}.webp`);
 </script>
 
 <style lang="scss" scoped>
-@import '../assets/styles/colors';
+@use '../assets/styles/colors' as *;
 
-.header {
+.page-header {
+	align-items: center;
 	background-image: url('/assets/images/patterns/header-bg-pattern.webp');
 	background-repeat: repeat-x;
 	background-size: auto 100%;
-	height: 72px;
 	display: flex;
-	justify-content: space-between;
-	align-items: center;
 	gap: 32px;
-}
+	height: 72px;
+	justify-content: space-between;
 
-.header .nav {
-	align-items: center;
-	display: flex;
-	font-family: 'Solitreo', sans-serif;
-	gap: 16px;
-	height: 100%;
-	justify-content: center;
-}
+	& .page-navigation {
+		align-items: center;
+		display: flex;
+		gap: 16px;
+		height: 100%;
+		justify-content: center;
 
-.header .header-column {
-	flex-grow: 1;
-	text-align: right;
-	display: flex;
-	justify-content: flex-end;
-}
+		& .link {
+			color: $text-color-main;
+			font-size: 20px;
+			line-height: 24px;
+			text-decoration: none;
 
-.header .nav .link {
-	color: $text-color-main;
-	text-decoration: none;
-	font-size: 20px;
-	line-height: 24px;
+			&:hover {
+				color: $text-color-hover;
+			}
+		}
+	}
 
-	&:hover {
-		color: $text-color-hover;
+	& .header-column {
+		display: flex;
+		flex-grow: 1;
+		justify-content: flex-end;
+		text-align: right;
 	}
 }
 </style>
