6bdea1be842a128dcdb91c69dbb21c0d23cdd5ff
Ivett LATÁK <ivett.latak@shiwaforce.com>
2024-10-27 15:12:00 +0100

    feat: add home tile illustration

--- karutabaka/src/views/HomeView.vue
+++ karutabaka/src/views/HomeView.vue
@@ -12,25 +12,25 @@ const HOME_TILES: HomeTileProps[] = [
 		routeName: 'learning',
 		title: 'Learning',
 		description: 'Access learning materials and tutorials to improve your karuta skills.',
-		iconSrc: '/assets/images/bara-head-momiji.webp',
+		iconSrc: '/assets/images/tile-icons/bara-learning.webp',
 	},
 	{
 		routeName: 'gallery',
 		title: 'Gallery',
 		description: 'Explore the collection of the 100 karuta cards.',
-		iconSrc: '/assets/images/bara-head.webp',
+		iconSrc: '/assets/images/tile-icons/bara-magnifying.webp',
 	},
 	{
 		routeName: 'reciter',
 		title: 'Reciter',
 		description: 'Utilize the reciter program for gameplay or to improve your listening and reading skills.',
-		iconSrc: '/assets/images/bara-head-momiji.webp',
+		iconSrc: '/assets/images/tile-icons/bara-singing.webp',
 	},
 	{
 		routeName: 'rules',
 		title: 'Rules',
 		description: 'Learn the official rules and guidelines of Hyakunin Isshu Kyougi Karuta.',
-		iconSrc: '/assets/images/bara-head.webp',
+		iconSrc: '/assets/images/tile-icons/bara-judge.webp',
 	},
 ];
 </script>
